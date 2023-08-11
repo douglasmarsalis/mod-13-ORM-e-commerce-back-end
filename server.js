193f5ed6c7ade050f75fdf5f33a1dc2948696ef3
Douglas <japanmarsalis@gmail.com>
2023-08-11 13:10:05 -0500

    Added .env info, imported sequelize connection- added code, synced models to database- added code

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('sequelize');
+
+// Import sequelize connection
+const sequelize = require('./config/connection'); // Added this line of code
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// Sync sequelize models to the database, then turn on the server
+sequelize.sync().then(() => { // Added this line of code plus combined with original code below
+  app.listen(PORT, () => console.log(`App is listening on port ${PORT}!`));
 });
+
+
+// BELOW WAS PART OF ORIGINAL CODE 
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
